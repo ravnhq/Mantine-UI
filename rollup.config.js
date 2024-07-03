c38deecdd451b64739268effdbf041448f27768f
Fer Santamaria <fer.santamaria@hotmail.com>
2024-07-03 17:30:55 -0600

    chore: adds bundler config

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+import terser from "@rollup/plugin-terser"
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
+import peerDepsExternal from "rollup-plugin-peer-deps-external"
+import postcss from "rollup-plugin-postcss"
+
+const packageJson = require("./package.json")
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.rollup.json" }),
+      terser(),
+      postcss({
+        extract: true,
+        minimize: true,
+        use: [
+          [
+            "sass",
+            {
+              data: '@import "./_mantine.scss"; ',
+            },
+          ],
+        ],
+      }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [dts.default()],
+    external: [/\.css$/],
+  },
+]
