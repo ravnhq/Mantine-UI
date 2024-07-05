8f95e741ac137a7e5db8c8c11b22d961ef366af3
keviningles2314 <63429867+keviningles2314@users.noreply.github.com>
2024-07-05 17:58:42 -0600

    feat(MANUI-13): Improvement generic header component (#5)

--- src/components/headers/header-mega-menu/header-mega-menu.types.ts
+++ src/components/headers/header-mega-menu/header-mega-menu.types.ts
@@ -1,23 +1,7 @@
-import { type ButtonProps } from "@mantine/core"
+import { ReactNode } from "react"
 
-interface ISubMenudata {
-  // Todo: Discuss type for icons
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  icon: any
-  title: string
-  description: string
-}
-
-interface ImenuData {
-  title: string
-  isSubMenu: boolean
-}
-
-export type HeaderWithMegaMenuProps = {
-  primaryButtontitle?: string
-  primaryButtonProps?: ButtonProps
-  secondarybuttontitle?: string
-  secondaryButtonProps?: ButtonProps
-  subMenuItems: ISubMenudata[]
-  menuItems: ImenuData[]
+export interface HeaderWithMegaMenuProps {
+  logo?: ReactNode
+  rightContent?: ReactNode
+  menuItems: ReactNode[]
 }
